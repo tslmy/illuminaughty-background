e1de9f41174aabf1a2234d556a49e15c9b405526
tslmy <tslimingyang@126.com>
2015-10-04 14:24:28 -0400

    update procfile

--- main.py
+++ main.py
@@ -171,37 +171,31 @@ def DEBUGdumpTheBSide():
 	f = open('dump.txt','w+')
 	f.write(json.dumps(shortestPaths, sort_keys=True))
 	f.close()
-if __name__=='__main__':
-	#findPath('Q7802')
-	#print naturallyDescribeWithClaims(bestAnswer)
-	from flask import Flask, session, render_template, request, jsonify, redirect, url_for, send_from_directory
-	app = Flask(__name__)
-	# Sessions variables are stored client side, on the users browser. The content of the variables is encrypted, so users can't
-	# actually see it. They could edit it, but again, as the content wouldn't be signed with this hash key, it wouldn't be valid
-	# You need to set a secret key (random text) and keep it secret.
-	app.secret_key = 'iu4hntvw8h7ft'
-	#@app.route('/')
-	#def index():
-	#	return render_template('index.html')
-	@app.route('/run')
-	def run():
-		userInput = request.args.get('query', '')
-		print 'userInput:',userInput
-		userInputAsId = wikidataSearch(userInput)
-		if userInputAsId=='': #no such thing!
-			return jsonify(resultInIds=[], resultInLabels=[], naturalDescription='No such thing.')
+
+from flask import Flask, render_template, request, jsonify#, redirect, url_for, send_from_directory
+app = Flask(__name__)
+@app.route('/')
+#def index():
+#	return render_template('index.html')
+#@app.route('/run')
+def index():
+	userInput = request.args.get('q', '')
+	print 'userInput:',userInput
+	userInputAsId = wikidataSearch(userInput)
+	if userInputAsId=='': #no such thing!
+		return jsonify(resultInIds=[], resultInLabels=[], naturalDescription='No such thing.')
+	else:
+		print 'userInputAsId:',userInputAsId
+		findPath(userInputAsId)
+		if bestAnswer==[]:
+			return jsonify(resultInIds=[], resultInLabels=[], naturalDescription='No relationship found.')
 		else:
-			print 'userInputAsId:',userInputAsId
-			findPath(userInputAsId)
-			if bestAnswer==[]:
-				return jsonify(resultInIds=[], resultInLabels=[], naturalDescription='No relationship found.')
-			else:
-				return jsonify(resultInIds=bestAnswer, resultInLabels=convertClaimsFromIdsToLabels(bestAnswer), naturalDescription=naturallyDescribeWithClaims(bestAnswer))
-	app.run(host="0.0.0.0",port=int("1336"),debug=True,threaded=True)
-	##DEBUGdumpTheBSide()
-	#Set up the language checker:
-	#languageTool = language_check.LanguageTool('en-CA')
-	#Fix the language:
-	#languageCheckerMatches = languageTool.check(naturalDescription)
-	#naturalDescription = language_check.correct(naturalDescription, languageCheckerMatches)
-	#print naturalDescription
+			return jsonify(resultInIds=bestAnswer, resultInLabels=convertClaimsFromIdsToLabels(bestAnswer), naturalDescription=naturallyDescribeWithClaims(bestAnswer))
+app.run(host="0.0.0.0",port=int("80"),debug=False)#,threaded=True)
+##DEBUGdumpTheBSide()
+#Set up the language checker:
+#languageTool = language_check.LanguageTool('en-CA')
+#Fix the language:
+#languageCheckerMatches = languageTool.check(naturalDescription)
+#naturalDescription = language_check.correct(naturalDescription, languageCheckerMatches)
+#print naturalDescription
